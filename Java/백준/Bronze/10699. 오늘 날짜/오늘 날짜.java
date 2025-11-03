94fce2c0b12ce80a6d0324cf0b2f26f40547f673
강민재 <rkdalswoals@gmail.com>
2025-11-03 23:23:11 +0900

    [Bronze V] Title: 오늘 날짜, Time: 180 ms, Memory: 18328 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,13 @@
+//  오늘 날짜
+
+import java.time.LocalDate;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(LocalDate.now());
+    }
+}
