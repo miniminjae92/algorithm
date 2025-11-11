aa2e5ad9a1699243c5347312fa5178aba565e1bc
Minjae Kang <rkdalswoals@gmail.com>
2025-11-11 22:45:30 +0900

    [level 0] Title: 수 조작하기 2, Time: 7.78 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numLog.length - 1; i++) {
+            int gap = numLog[i + 1] - numLog[i];
+            if (gap == 1) {
+                sb.append('w');
+            } else if (gap == 10) {
+                sb.append('d');
+            } else if (gap == -1) {
+                sb.append('s');
+            } else if (gap == -10) {
+                sb.append('a');
+            }
+        }
+        return sb.toString();
+    }
+}
+
