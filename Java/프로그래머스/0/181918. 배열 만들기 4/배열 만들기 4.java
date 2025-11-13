73947c4d20cf61250e3e9ce4d1c4d55a661f0a8b
Minjae Kang <rkdalswoals@gmail.com>
2025-11-13 22:43:51 +0900

    [level 0] Title: 배열 만들기 4, Time: 22.60 ms, Memory: 101 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> stk = new ArrayList<>();
+        int i = 0;
+        int arrLen = arr.length;
+        while (i < arrLen) {
+            if (stk.isEmpty()) {
+                stk.add(arr[i]);
+                i++;
+            }
+            if (stk.get(stk.size() - 1) < arr[i]) {
+                stk.add(arr[i]);
+                i++;
+            } else {
+                stk.remove(stk.size() - 1);
+            }
+        }
+        return stk.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
