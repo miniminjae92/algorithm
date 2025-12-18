fca65dafd455355ff25c2901fdfcd3320e5f1305
Minjae Kang <rkdalswoals@gmail.com>
2025-12-18 15:08:42 +0900

    [level 0] Title: 1로 만들기, Time: 0.03 ms, Memory: 68.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ Java/프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -2,14 +2,9 @@ class Solution {
     public int solution(int[] num_list) {
         int count = 0;
         for (int n : num_list) {
-            while (n != 1) {
-                if (n % 2 == 0) {
-                    n /= 2;
-                    count++;
-                } else {
-                    n = (n - 1) / 2;
-                    count++;
-                }
+            while (n > 1) {
+                n >>= 1;
+                count++;
             }
         }
         return count;
