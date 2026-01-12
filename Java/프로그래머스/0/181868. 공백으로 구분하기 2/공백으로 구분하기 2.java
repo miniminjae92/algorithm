a565d9c3c1b28f30d3c31840fd8a5205f865aa40
Minjae Kang <rkdalswoals@gmail.com>
2026-01-12 10:41:07 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 3.00 ms, Memory: 78 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+        public String[] solution(String my_string) {
+            return Arrays.stream(my_string.split(" "))
+                    .filter(s -> !s.isBlank())
+                    .toArray(String[]::new);
+        }
+}
