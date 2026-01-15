ba6eb2f3aa0a80a245b5b93d37501f143345f49d
Minjae Kang <rkdalswoals@gmail.com>
2026-01-15 20:52:07 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 8.43 ms, Memory: 83.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ Java/프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -1,7 +1,17 @@
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        return Arrays.stream(num_list).sorted().limit(5).toArray();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int num : num_list) {
+            pq.add(num);
+            if (pq.size() > 5) {
+                pq.poll();
+            }
+        }
+        
+        return pq.stream().mapToInt(Integer::intValue).sorted().toArray();
     }
 }
