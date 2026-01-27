701d1858251198c2d9ce3701c27c753875f6d925
Minjae Kang <rkdalswoals@gmail.com>
2026-01-27 09:25:45 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,10 @@
+class Solution {
+    private final static int AMERICANO = 5_500;
+    
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        answer[0] = money / AMERICANO;
+        answer[1] = money % AMERICANO;
+        return answer;
+    }
+}
