fe5e9b45debb83718321707c32eefe89e70f9daa
Minjae Kang <rkdalswoals@gmail.com>
2026-03-16 17:47:47 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int angle) {
+        if (0 < angle && angle < 90) {
+            return 1;
+        }
+        if (angle == 90) {
+            return 2;
+        }
+        if (angle < 180) {
+            return 3;
+        }
+        if (angle == 180) {
+            return 4;
+        }
+        return 0;
+    }
+}
