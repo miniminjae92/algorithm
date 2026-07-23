37bd251ca44b216c4c1137603b000c5d9542d137
Minjae Kang <rkdalswoals@gmail.com>
2026-07-23 22:56:47 +0900

    chore: baekjoon 폴더의 초기 연습 코드 3개 병합 (정리 작업 중 회수)

---
+++ Java/백준/_초기연습/HelloWorld.java
@@ -0,0 +1,12 @@
+//  Hello World
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello World!");
+    }
+}
